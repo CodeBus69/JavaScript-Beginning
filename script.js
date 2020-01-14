038a5cae2918dbef82e6e49a12deb3d85178ff58
CodeBus69 <kucharczyk.main@gmail.com>
2020-01-14 03:33:28 +0100

    act

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Nasz skrypt dziala i ma sie swietnie! :D ");
+
+///////////////////////////////////////////////////////////
+
+for (let i=1; i<=10;i++) { 
+console.log(i);}
+
+let text1 = 'nasz tekst'; 
+console.log("Dlugosc " , text1 , " wynosi " , text1.length);
+
+
+
